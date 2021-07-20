ba6ef87ba50e2a5a3771c54c7aec717ef0674a38
Hozefa Mangalorewala <hozefam@gmail.com>
2021-07-20 12:04:24 +0300

    Added model and routes

--- app.js
+++ app.js
@@ -10,20 +10,14 @@ const api = process.env.API_URL;
 app.use(express.json());
 app.use(morgan('tiny'));
 
-app.get(`${api}/products`, (req, res) => {
-  const product = {
-    id: 1,
-    name: 'hair dresser',
-    image: 'some_url',
-  };
-  res.send(product);
-});
-
-app.post(`${api}/products`, (req, res) => {
-  const newProduct = req.body;
-  console.log(newProduct);
-  res.send(newProduct);
-});
+const productsRouter = require('./routers/products');
+const usersRouter = require('./routers/users');
+const ordersRouter = require('./routers/orders');
+const categoryRouter = require('./routers/cateogories');
+app.use(`${api}/products`, productsRouter);
+app.use(`${api}/users`, usersRouter);
+app.use(`${api}/orders`, ordersRouter);
+app.use(`${api}/categories`, categoryRouter);
 
 mongoose
   .connect(process.env.CONNECTION_STRING, {
@@ -32,7 +26,7 @@ mongoose
     dbName: 'eshop-database',
   })
   .then(() => console.log('Database Connection is ready'))
-  .catch((err) => console.log(err));
+  .catch(err => console.log(err));
 
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
---
+++ routers/products.js
@@ -0,0 +1,34 @@
+const { Product } = require('../models/product');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const productList = await Product.find({});
+
+  if (!productList) {
+    res.status(500).json({
+      success: false,
+    });
+  }
+
+  res.send(productList);
+});
+
+router.post('/', (req, res) => {
+  const { name, image, countInStock } = req.body;
+  const product = new Product({ name, image, countInStock });
+  product
+    .save()
+    .then(createdProduct => {
+      res.status(201).json(createdProduct);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err,
+        success: false,
+      });
+    });
+});
+
+module.exports = router;
